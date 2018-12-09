dbb04ef490c87db87528d0a32693fbffc60d6884
KIMINJOONG <ijboym216@gmail.com>
2018-12-09 20:15:43 +0900

    update App and Weather js

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
 import React, {Component } from 'react';
-import { StyleSheet, Text, View, Image, ActivityIndicator } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import Weather from "./Weather";
+
+const API_KEY = "cf01fab376de253a0e8ac6d98af2bdce";
 
 
 export default class App extends Component {
   state = {
-    isLoaded: true
+    isLoaded: false
+    , error:null
+    , temperature: null
+    , name:null
+  };
+  componentDidMount(){
+    navigator.geolocation.getCurrentPosition(
+      position => {
+          this._getWeather(position.coords.latitude, position.coords.longitude)
+      }
+      , error => {
+        this.setState({
+          error: error
+        });
+      }
+    );
+  }
+
+  _getWeather = (lat, long) => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=${API_KEY}`)
+    .then(response => response.json())
+    .then(json => {
+      this.setState({
+        temperature: json.main.temp
+        , name: json.weather[0].main
+        , isLoaded: true
+      });
+    })
   }
   render() {
-    const { isLoaded } = this.state;
+    const { isLoaded, error, temperature, name } = this.state;
     return (
       <View style={styles.container}>
-        
-          {isLoaded ? <Weather /> : (
+          <StatusBar hidden={true} />
+          {isLoaded ? <Weather weatherName={name} temp={Math.floor(temperature - 273.15)} /> : (
           <View style={styles.loading}>
             <Text style={styles.loadingText}>Getting the fucking weather</Text>
+            {error ? <Text style={styles.errorText}>{error}</Text> : null}
           </View>)}
         
       </View>
@@ -27,6 +57,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff'
   },
+  errorText: {
+    color: "red"
+    , backgroundColor: "transparent"
+    , marginBottom: 40
+  },
 
   loading: {
     flex: 1
@@ -36,7 +71,7 @@ const styles = StyleSheet.create({
   },
   loadingText: {
     fontSize: 38
-    , marginBottom: 100
+    , marginBottom: 24
   }
   
 });
--- Weather.js
+++ Weather.js
@@ -1,26 +1,79 @@
 import React, {Component}  from 'react';
 import { StyleSheet, Text, View, Image, ActivityIndicator } from 'react-native';
 import { LinearGradient } from "expo";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import PropTypes from "prop-types";
 
-export default class Weather extends Component {
-    render() {
-        return (
+const weatherCase = {
+    Rain: {
+        colors:[ "#00C6FB", "#005BEA"]
+        , title: "Raining like a MF"
+        , subtitle: "For more info look outside"
+        , icon: "ios-rainy"
+    }
+    , Clear: {
+        colors:[ "#FEF253", "#FF7300"]
+        , title: "Sunny as Fuck"
+        , subtitle: "Go get your ass  burnt"
+        , icon: "ios-sunny"
+    }
+    , Thunderstorm: {
+        colors:[ "#00ECBC", "#007ADF"]
+        , title: "Thunderstorm in the house"
+        , subtitle: "Actually, outside of the house"
+        , icon: "ios-thunderstorm"
+    }
+    , Clouds: {
+        colors:[ "#D7D2CC", "#304352"]
+        , title: "Clouds"
+        , subtitle: "I know, fucking boring"
+        , icon: "ios-cloudy"
+    }
+    , Snow: {
+        colors:[ "#7DE2FC", "#B9B6E5"]
+        , title: "Cold as balls"
+        , subtitle: "Do you want to build a snowman? Fuck no."
+        , icon: "ios-snow"
+    }
+    , Drizzle: {
+        colors:[ "#89F7FE", "#66A6FF"]
+        , title: "Drizzle"
+        , subtitle: "Is like rain, but gay"
+        , icon: "ios-rainy-outline"
+    }
+    , Haze: {
+        colors:[ "#89F7FE", "#66A6FF"]
+        , title: "Drizzle"
+        , subtitle: "Is like rain, but gay"
+        , icon: "ios-rainy-outline"
+    }
+}
+
+function Weather({weatherName, temp}) {
+    console.log(weatherName);
+    return (
         <LinearGradient 
-            colors={["#00C6FB", "#005BEA"]} 
+            colors={weatherCase[weatherName].colors} 
             style={styles.container} 
             >
             <View style={styles.upper}>
-                <Text>Icon here!</Text>
-                <Text style={styles.temp}>35</Text>
+                <MaterialCommunityIcons color="white" size={144} name={weatherCase[weatherName].icon} />
+                <Text style={styles.temp}>{temp}</Text>
             </View>
             <View style={styles.lower}>
-                <Text style={styles.title}>Raining like a MF</Text>
-                <Text style={styles.subtitle}>For more info look outside</Text>
+                <Text style={styles.title}>{weatherCase[weatherName].title}</Text>
+                <Text style={styles.subtitle}>{weatherCase[weatherName].subtitle}</Text>
             </View>
         </LinearGradient>
             );
-    }
 }
+
+Weather.PropTypes = {
+    temp: PropTypes.number.isRequired
+    , weatherName: PropTypes.string.isRequired
+}
+
+export default Weather;
 
 const styles= StyleSheet.create({
     container: {
@@ -30,9 +83,10 @@ const styles= StyleSheet.create({
         flex: 1
         , alignItems: "center"
         , justifyContent:"center"
+        , backgroundColor : "transparent"
     }
     , temp: {
-        fontSize: 38
+        fontSize: 48
         , backgroundColor: "transparent"
         , color: "white"
         , marginTop: 10
