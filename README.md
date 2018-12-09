08ce0f33197a48f912c206a869a1f3756efce3ea
KIMINJOONG <ijboym216@gmail.com>
2018-12-09 20:23:21 +0900

    error memo in READ ME

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # tutorial-weather-withReactNative
 my first reactNative
+
+#안드로이드 에뮬레이터로는 되는데 QR코드로 했을때 안됄시
+ipconfig 으로 IPv4 주소 메모
+해당 프로젝트로 가서
+set REACT_NATIVE_PACKAGER_HOSTNAME=IPv4주소
+그다음 expo start로 실행
--- Weather.js
+++ Weather.js
@@ -9,43 +9,49 @@ const weatherCase = {
         colors:[ "#00C6FB", "#005BEA"]
         , title: "Raining like a MF"
         , subtitle: "For more info look outside"
-        , icon: "ios-rainy"
+        , icon: "weather-rainy"
     }
     , Clear: {
         colors:[ "#FEF253", "#FF7300"]
         , title: "Sunny as Fuck"
         , subtitle: "Go get your ass  burnt"
-        , icon: "ios-sunny"
+        , icon: "weather-sunny"
     }
     , Thunderstorm: {
         colors:[ "#00ECBC", "#007ADF"]
         , title: "Thunderstorm in the house"
         , subtitle: "Actually, outside of the house"
-        , icon: "ios-thunderstorm"
+        , icon: "weather-lightning"
     }
     , Clouds: {
         colors:[ "#D7D2CC", "#304352"]
         , title: "Clouds"
         , subtitle: "I know, fucking boring"
-        , icon: "ios-cloudy"
+        , icon: "weather-cloudy"
     }
     , Snow: {
         colors:[ "#7DE2FC", "#B9B6E5"]
         , title: "Cold as balls"
         , subtitle: "Do you want to build a snowman? Fuck no."
-        , icon: "ios-snow"
+        , icon: "weather-snowy"
     }
     , Drizzle: {
         colors:[ "#89F7FE", "#66A6FF"]
         , title: "Drizzle"
         , subtitle: "Is like rain, but gay"
-        , icon: "ios-rainy-outline"
+        , icon: "weather-hail"
     }
     , Haze: {
         colors:[ "#89F7FE", "#66A6FF"]
-        , title: "Drizzle"
-        , subtitle: "Is like rain, but gay"
-        , icon: "ios-rainy-outline"
+        , title: "Haze"
+        , subtitle: "Dont know what that is"
+        , icon: "weather-hail"
+    }
+    , Mist: {
+        colors:[ "#89F7FE", "#66A6FF"]
+        , title: "Mist!"
+        , subtitle: "It's like you have no glasses on."
+        , icon: "weather-mist"
     }
 }
 
